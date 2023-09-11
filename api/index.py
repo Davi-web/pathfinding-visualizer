3a0c6cbe74819fa196439b188849939da4a425ff
Davi-web <jeseung.ha@vanderbilt.edu>
2023-09-11 02:15:08 -0700

    a

--- api/index.py
+++ api/index.py
@@ -23,20 +23,29 @@ def hello_world():
 def algorithms(req: Body):
     start, end, algorithm = req.start, req.end, req.algorithm
     board = req.board
+    path = []
+    if algorithm == 'bfs':
+        path = bfs(board, start)
+    elif algorithm == 'dfs':
+        path = dfs(board, start)
+    elif algorithm == 'dijkstra':
+        path = dijkstra(board, start, end)
+    elif algorithm == 'astar':
+        path = astar(board, start, end)
 
-    match algorithm:
-        case 'bfs':
-            print('Performing BFS Algorithm')
-            path = bfs(board, start)
-        case 'dijkstra':
-            print('Perfroming Dijkstra Algorithm')
-            path = dijkstra(board, start, end)
-        case 'astar':
-            print('Performing A* Algorithm')
-            path = astar(board, start, end)
-        case _:
-            print('Performing DFS Algorithm')
-            path = dfs(board, start)
+    # match algorithm:
+    #     case 'bfs':
+    #         print('Performing BFS Algorithm')
+    #         path = bfs(board, start)
+    #     case 'dijkstra':
+    #         print('Perfroming Dijkstra Algorithm')
+    #         path = dijkstra(board, start, end)
+    #     case 'astar':
+    #         print('Performing A* Algorithm')
+    #         path = astar(board, start, end)
+    #     case _:
+    #         print('Performing DFS Algorithm')
+    #         path = dfs(board, start)
     return {
         'path': path,
         'valid': len(path) > 0
