389eb791ba379d4705a014fdb7080560c37c0169
Davi-web <jeseung.ha@vanderbilt.edu>
2023-09-20 13:38:48 -0700

    changed nextjs config file

--- next.config.js
+++ next.config.js
@@ -1,29 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/docs',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/docs'
-            : '/api/docs',
-      },
-      {
-        source: '/openapi.json',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/openapi.json'
-            : '/api/openapi.json',
-      },
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/:path*'
-            : '/api/',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/docs',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.01:8000/docs'
+              : '/api/docs',
+        },
+
+        {
+          source: '/openapi.json',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.01:8000/openapi.json'
+              : '/api/openapi.json',
+        },
+      ],
+      afterFiles: [
+        {
+          source: '/api/:path*',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.01:8000/api/:path*'
+              : '/api/',
+        },
+      ],
+
+      // {
+      //   source: '/docs',
+      //   destination:
+      //     process.env.NODE_ENV === 'development'
+      //       ? 'http://127.0.0.1:8000/docs'
+      //       : '/api/docs',
+      // },
+      // {
+      //   source: '/openapi.json',
+      //   destination:
+      //     process.env.NODE_ENV === 'development'
+      //       ? 'http://127.0.0.1:8000/openapi.json'
+      //       : '/api/openapi.json',
+      // },
+      // {
+      //   source: '/api/:path*',
+      //   destination:
+      //     process.env.NODE_ENV === 'development'
+      //       ? 'http://127.0.0.1:8000/api/:path*'
+      //       : '/api/',
+      // },
+    };
   },
 };
 
