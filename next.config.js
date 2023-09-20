dd9c964b280b4f556bfb56e5ae98fdb00474f2a3
Davi-web <jeseung.ha@vanderbilt.edu>
2023-09-20 13:31:39 -0700

    a

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:8000/docs'
-            : '/api/docs',
+            : '/docs',
       },
       {
         source: '/openapi.json',
