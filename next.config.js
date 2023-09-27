0de03319878c112749f036c18f3bb4987fe98a26
Davi-web <jeseung.ha@vanderbilt.edu>
2023-09-26 18:44:29 -0700

    fixing nextjs config file

--- next.config.js
+++ next.config.js
@@ -1,38 +1,67 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/docs',
-        // destination: '/api/docs',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/docs'
-            : '/api/docs',
-      },
-      {
-        source: '/redoc',
-        // destination: '/api/redoc',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/redoc'
-            : '/api/redoc',
-      },
-      {
-        source: '/openapi.json',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/openapi.json'
-            : '/api/openapi.json',
-      },
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/:path*'
-            : '/api/',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/api/:path*'
+              : '/api/',
+        },
+      ],
+      afterFiles: [
+        {
+          source: '/docs',
+          // destination: '/api/docs',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/docs'
+              : '/api/docs',
+        },
+        {
+          source: '/redoc',
+          // destination: '/api/redoc',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/redoc'
+              : '/api/redoc',
+        },
+        {
+          source: '/openapi.json',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/openapi.json'
+              : '/api/openapi.json',
+        },
+      ],
+      fallback: [
+        {
+          source: '/docs',
+          // destination: '/api/docs',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/docs'
+              : '/api/docs',
+        },
+        {
+          source: '/redoc',
+          // destination: '/api/redoc',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/redoc'
+              : '/api/redoc',
+        },
+        {
+          source: '/openapi.json',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:8000/openapi.json'
+              : '/api/openapi.json',
+        },
+      ],
+    };
   },
 };
 
