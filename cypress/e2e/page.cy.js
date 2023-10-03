748fb9a354a63ab51a4aa2176b7a3455b98940ed
Davi-web <jeseung.ha@vanderbilt.edu>
2023-10-03 03:27:17 -0700

    Add more descriptive test names for cypress

--- cypress/e2e/page.cy.js
+++ cypress/e2e/page.cy.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://pathfinding-visualizer-janq.vercel.app");
-    const description = cy.get("p .text-left");
-    description.should("have.length", "1");
+describe('Home Page Spec', () => {
+  it('Should render html with 10 buttons', () => {
+    cy.visit('https://pathfinding-visualizer-janq.vercel.app');
+    const description = cy.get('p .text-left');
+    description.should('have.length', '1');
     description.should(
-      "have.html",
-      "This is a pathfinding visualizer that allows you to find the path between the start and end node. Add walls or generate a random board to visualize the path",
+      'have.html',
+      'This is a pathfinding visualizer that allows you to find the path between the start and end node. Add walls or generate a random board to visualize the path'
     );
 
-    const buttons = cy.get("button");
-    buttons.should("have.length", 10);
+    const buttons = cy.get('button');
+    buttons.should('have.length', 10);
   });
 });
