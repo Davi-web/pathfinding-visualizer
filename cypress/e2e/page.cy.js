951a655acbaa3ede4f751f3a243d1257e32ebfd5
Davi-web <jeseung.ha@vanderbilt.edu>
2023-10-02 19:39:43 -0700

    update test url

--- cypress/e2e/page.cy.js
+++ cypress/e2e/page.cy.js
@@ -2,12 +2,15 @@
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000');
-    const description = cy.get("p .text-left")
-    description.should("have.length","1")
-    description.should("have.html","This is a pathfinding visualizer that allows you to find the path between the start and end node. Add walls or generate a random board to visualize the path");
+    cy.visit('https://pathfinding-visualizer-janq.vercel.app');
+    const description = cy.get('p .text-left');
+    description.should('have.length', '1');
+    description.should(
+      'have.html',
+      'This is a pathfinding visualizer that allows you to find the path between the start and end node. Add walls or generate a random board to visualize the path'
+    );
 
-    const buttons = cy.get("button");
-    buttons.should("have.length", 10);
-  })
-})
+    const buttons = cy.get('button');
+    buttons.should('have.length', 10);
+  });
+});
